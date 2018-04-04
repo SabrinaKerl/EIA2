5a3ca63f42c6d9acf3723360c4bab629ed07e449
Sabrina K <Sabrina K@141.28.229.28>
2018-04-04 23:31:39 +0200

    Aufgabe 1

--- Aufgabe 0/Aufgabe0.js
+++ Aufgabe 0/Aufgabe0.js
@@ -1,6 +1,7 @@
-function box(){
-    var input = prompt ("Hi, verrätst Du mir Deinen Namen?", "");
-    if (input != null){
-        document.getElementById ("Hi").innerHTML = "Danke Dir " + input;
-        }
+function box() {
+    var input = prompt("Hi, verr�tst Du mir Deinen Namen?", "");
+    if (input != null) {
+        document.getElementById("Hi").innerHTML = "Danke Dir " + input;
     }
+}
+//# sourceMappingURL=Aufgabe0.js.map
